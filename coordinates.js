7e608d4fe0ed12921d353d2c3f58b1c1d0fa5fd2
inventivetalent <mail@inventivetalent.org>
2020-07-05 14:59:17 +0200

    mccapes coordinates

--- coordinates.js
+++ coordinates.js
@@ -6,5 +6,9 @@ module.exports = {
     "labymod": {
         dynamic: true, // coordinates are percentage of image dimensions
         front: [1 / 22, 0, 0.485, 1] // Why are you so weird?
-    }
+    },
+    "minecraftcapes": {
+        dynamic: true,
+        front: [1/64, 1/32, 10/64,16/32]
+    },
 }
