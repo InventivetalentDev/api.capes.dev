ad4cf63d3f0bdf3f011e348d1e0a86386b2390d1
inventivetalent <mail@inventivetalent.org>
2020-07-05 19:00:51 +0200

    remove debugs

--- loaders/minecraft.js
+++ loaders/minecraft.js
@@ -14,16 +14,13 @@ class MinecraftCapeLoader extends CapeLoader {
             console.log("GET " + url);
             axios.get(url).then(resp=>{
                 let profile = resp.data;
-                console.log(profile);
                 if (profile && profile.properties) {
                     if (profile.properties.length > 0) {
                         let base64 = profile.properties[0].value;
                         let decoded = new Buffer(base64, 'base64').toString('ascii');
                         let textureInfo = JSON.parse(decoded);
-                        console.log(textureInfo);
                         if (textureInfo.textures && textureInfo.textures.CAPE) {
                             let cape = textureInfo.textures.CAPE;
-                            console.log(cape);
                             if (cape.url) {
                                 this.loadCapeImage(cape.url).then(resolve);
                                 return;
