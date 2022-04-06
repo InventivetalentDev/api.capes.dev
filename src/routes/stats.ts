da8200aa754a62f6bdd09a5a2880d01bf12f9598
inventivetalent <mail@inventivetalent.org>
2022-04-06 17:09:22 +0200

    .

--- src/routes/stats.ts
+++ src/routes/stats.ts
@@ -25,21 +25,21 @@ export const register = (app: Application) => {
 
         const totalCount = await Cape.countDocuments({ imageHash: { $ne: HAS_NO_CAPE } }).exec();
         const distinctPlayerCount = await Cape.aggregate([{ $group: { _id: "$player" } }, { $count: "count" }]).exec().then((docs: any[]) => docs[0]["count"]);
-        // const perTypeCount = await Cape.aggregate([{ $match: { imageHash: { $ne: HAS_NO_CAPE } } }, { $group: { _id: '$type', count: { $sum: 1 } } }]).exec()
-        //     .then((perType: any[]) => {
-        //         let types: { [s: string]: number } = {};
-        //         for (let t of perType) {
-        //             types[t["_id"]] = Math.floor(t["count"]);
-        //         }
-        //         return types;
-        //     });
+        const perTypeCount = await Cape.aggregate([{ $match: { imageHash: { $ne: HAS_NO_CAPE } } }, { $group: { _id: '$type', count: { $sum: 1 } } }]).exec()
+            .then((perType: any[]) => {
+                let types: { [s: string]: number } = {};
+                for (let t of perType) {
+                    types[t["_id"]] = Math.floor(t["count"]);
+                }
+                return types;
+            });
 
-        const perTypeCount: {[type: string]: number} = {};
-        const typePromises = [];
-        for (let type of Object.values(CapeType)) {
-            typePromises.push(Cape.countDocuments({ imageHash: { $ne: HAS_NO_CAPE }, type: type }).exec().then(c => perTypeCount[type] = c));
-        }
-        await Promise.all(typePromises);
+        // const perTypeCount: {[type: string]: number} = {};
+        // const typePromises = [];
+        // for (let type of Object.values(CapeType)) {
+        //     typePromises.push(Cape.countDocuments({ imageHash: { $ne: HAS_NO_CAPE }, type: type }).exec().then(c => perTypeCount[type] = c));
+        // }
+        // await Promise.all(typePromises);
 
 
         stats.total = totalCount;
