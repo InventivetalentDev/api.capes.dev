6eeb6ccb0703015b669bc1956e3d278c25145b33
Haylee Schäfer <haylee@inventivetalent.org>
2024-07-31 08:15:51 +0200

    speed up CF migration

--- src/index.ts
+++ src/index.ts
@@ -174,7 +174,7 @@ async function init() {
     console.log("starting cloudflare migration task");
     setInterval(async () => {
         await migrateCapeToCloudflare();
-    }, 1000 * 45);
+    }, 1000 * 25);
     await migrateCapeToCloudflare();
 }
 
